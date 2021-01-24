714594097847f4338fa2da2cd9667051d87b6eff
isabellajoelle <55554631+isabellajoelle@users.noreply.github.com>
2021-01-24 15:38:39 -0500

    Update TicTacToe.py
    changed function names; changed method to detect a tie; cleaned EndgameChecker

--- TicTacToe.py
+++ TicTacToe.py
@@ -30,14 +30,14 @@ def intro():
     if wantAI():
         print()
         print("          YOU ARE PLAYER 1        ")
-        OnePlayerGame(start_board, [1, 2, 3, 4, 5, 6, 7, 8, 9], 1)
-    else: TwoPlayerGame(start_board, [1, 2, 3, 4, 5, 6, 7, 8, 9], 1)
+        OnePlayerGame(start_board, [1, 2, 3, 4, 5, 6, 7, 8, 9], 1, 1)
+    else: TwoPlayerGame(start_board, [1, 2, 3, 4, 5, 6, 7, 8, 9], 1, 1)
 
 def moveString(player):
     print()
     print("------------ PLAYER " + str(player) + " -------------")
 
-def TwoPlayerGame(game_board, board_tracker, player):
+def TwoPlayerGame(game_board, board_tracker, player, move_num):
     moveString(player)
     move = input("Enter Move #: ")
     while(not MoveValidityChecker(move, board_tracker)):
@@ -46,13 +46,13 @@ def TwoPlayerGame(game_board, board_tracker, player):
     print(new_board)
     if player == 1: board_tracker[int(move)-1] = True
     else: board_tracker[int(move)-1] = False
-    if endgame_checker(board_tracker) == "WIN": win(player)
-    elif endgame_checker(board_tracker) == "TIE": tie()
+    if EndgameChecker(board_tracker): win(player)
+    elif move_num == 9: tie()
     else:
-        if player == 1: TwoPlayerGame(new_board, board_tracker, 2)
-        else: TwoPlayerGame(new_board, board_tracker, 1) 
+        if player == 1: TwoPlayerGame(new_board, board_tracker, 2, move_num+1)
+        else: TwoPlayerGame(new_board, board_tracker, 1, move_num+1) 
 
-def OnePlayerGame(game_board, board_tracker, player):
+def OnePlayerGame(game_board, board_tracker, player, move_num):
     moveString(player)
     if player == 2:
         AI_move = makeAImove(game_board, board_tracker)
@@ -66,11 +66,11 @@ def OnePlayerGame(game_board, board_tracker, player):
         print(new_board)
     if player == 1: board_tracker[int(move)-1] = True
     else: board_tracker[AI_move[1]] = False
-    if endgame_checker(board_tracker) == "WIN": win(player)
-    elif endgame_checker(board_tracker) == "TIE": tie()
+    if EndgameChecker(board_tracker): win(player)
+    elif move_num == 9: tie()
     else:
-        if player == 1: OnePlayerGame(new_board, board_tracker, 2)
-        else: OnePlayerGame(AI_move[0], board_tracker, 1)
+        if player == 1: OnePlayerGame(new_board, board_tracker, 2, move_num+1)
+        else: OnePlayerGame(AI_move[0], board_tracker, 1, move_num+1)
 
 def wantAI():
     answer = input("         1 or 2 Players?: ")
@@ -105,14 +105,14 @@ def boardUpdater(current_board, position, player):
 
 def makeAImove(game_board, BT):
     #checks for 2 in a row
-    if two_checker(0, 1, 2, BT) < 10: return [boardUpdater(game_board, str(two_checker(0, 1, 2, BT)), 2), two_checker(0, 1, 2, BT)-1]
-    if two_checker(3, 4, 5, BT) < 10: return [boardUpdater(game_board, str(two_checker(3, 4, 5, BT)), 2), two_checker(3, 4, 5, BT)-1]
-    if two_checker(6, 7, 8, BT) < 10: return [boardUpdater(game_board, str(two_checker(6, 7, 8, BT)), 2), two_checker(6, 7, 8, BT)-1]
-    if two_checker(0, 3, 6, BT) < 10: return [boardUpdater(game_board, str(two_checker(0, 3, 6, BT)), 2), two_checker(0, 3, 6, BT)-1]
-    if two_checker(1, 4, 7, BT) < 10: return [boardUpdater(game_board, str(two_checker(1, 4, 7, BT)), 2), two_checker(1, 4, 7, BT)-1]
-    if two_checker(2, 5, 8, BT) < 10: return [boardUpdater(game_board, str(two_checker(2, 5, 8, BT)), 2), two_checker(2, 5, 8, BT)-1]
-    if two_checker(0, 4, 8, BT) < 10: return [boardUpdater(game_board, str(two_checker(0, 4, 8, BT)), 2), two_checker(0, 4, 8, BT)-1]
-    if two_checker(2, 4, 6, BT) < 10: return [boardUpdater(game_board, str(two_checker(2, 4, 6, BT)), 2), two_checker(2, 4, 6, BT)-1]
+    if TwoChecker(0, 1, 2, BT) < 10: return [boardUpdater(game_board, str(TwoChecker(0, 1, 2, BT)), 2), TwoChecker(0, 1, 2, BT)-1]
+    if TwoChecker(3, 4, 5, BT) < 10: return [boardUpdater(game_board, str(TwoChecker(3, 4, 5, BT)), 2), TwoChecker(3, 4, 5, BT)-1]
+    if TwoChecker(6, 7, 8, BT) < 10: return [boardUpdater(game_board, str(TwoChecker(6, 7, 8, BT)), 2), TwoChecker(6, 7, 8, BT)-1]
+    if TwoChecker(0, 3, 6, BT) < 10: return [boardUpdater(game_board, str(TwoChecker(0, 3, 6, BT)), 2), TwoChecker(0, 3, 6, BT)-1]
+    if TwoChecker(1, 4, 7, BT) < 10: return [boardUpdater(game_board, str(TwoChecker(1, 4, 7, BT)), 2), TwoChecker(1, 4, 7, BT)-1]
+    if TwoChecker(2, 5, 8, BT) < 10: return [boardUpdater(game_board, str(TwoChecker(2, 5, 8, BT)), 2), TwoChecker(2, 5, 8, BT)-1]
+    if TwoChecker(0, 4, 8, BT) < 10: return [boardUpdater(game_board, str(TwoChecker(0, 4, 8, BT)), 2), TwoChecker(0, 4, 8, BT)-1]
+    if TwoChecker(2, 4, 6, BT) < 10: return [boardUpdater(game_board, str(TwoChecker(2, 4, 6, BT)), 2), TwoChecker(2, 4, 6, BT)-1]
     #take middle if open
     if type(BT[4]) == int: return [boardUpdater(game_board, "5", 2), 4]
     #prevents open corner trap
@@ -125,7 +125,6 @@ def makeAImove(game_board, BT):
     if BT[7] == BT[5] and type(BT[5]) == bool:
         if type(BT[8]) == int: return [boardUpdater(game_board, "9", 2), 8]
     #prevents corner trap
-    if BT[4] == False and type(BT[1]) == int: return [boardUpdater(game_board, "2", 2), 1]
     if BT[0] == BT[8] and type(BT[0]) == bool:
         if type(BT[2]) == int: return [boardUpdater(game_board, "3", 2), 2]
         if type(BT[6]) == int: return [boardUpdater(game_board, "7", 2), 6]
@@ -133,13 +132,20 @@ def makeAImove(game_board, BT):
         if type(BT[0]) == int: return [boardUpdater(game_board, "1", 2), 0]
         if type(BT[8]) == int: return [boardUpdater(game_board, "9", 2), 8]
     else:
-        for i in range(9):
-            if type(BT[i]) == int:
-                move = i
-                break
-        return [boardUpdater(game_board, str(move+1), 2), move]
+        #first go for corners
+        if type(BT[0]) == int: return [boardUpdater(game_board, "1", 2), 0]
+        if type(BT[2]) == int: return [boardUpdater(game_board, "3", 2), 2]
+        if type(BT[6]) == int: return [boardUpdater(game_board, "7", 2), 6]
+        if type(BT[8]) == int: return [boardUpdater(game_board, "9", 2), 8]
+        #else make a random move
+        else:
+            for i in range(9):
+                if type(BT[i]) == int:
+                    move = i
+                    break
+            return [boardUpdater(game_board, str(move+1), 2), move]
         
-def two_checker(pos1, pos2, pos3, BT):
+def TwoChecker(pos1, pos2, pos3, BT):
     if type(BT[pos1]) == bool and type(BT[pos2]) == bool and type(BT[pos3])== bool: return 10
     if BT[pos1] == BT[pos2] and type(BT[pos1]) == bool:
         if type(BT[pos3]) == int: return BT[pos3]
@@ -149,21 +155,13 @@ def two_checker(pos1, pos2, pos3, BT):
         if type(BT[pos2]) == int: return BT[pos2]
     else: return 10
 
-def endgame_checker(BT):
-    if (BT[0] == BT[1] and BT[1] == BT[2]) or (BT[3] == BT[4] and BT[4] == BT[5]) or (BT[6] == BT[7] and BT[7] == BT[8]): return "WIN"
-    elif (BT[0] == BT[3] and BT[3] == BT[6]) or (BT[1] == BT[4] and BT[4] == BT[7]) or (BT[2] == BT[5] and BT[5] == BT[8]): return "WIN"
-    elif (BT[0] == BT[4] and BT[4] == BT[8]) or (BT[2] == BT[4] and BT[4] == BT[6]): return "WIN"
-    else:
-        tracker = True
-        for i in range(len(BT)):
-            tracker += 1
-            if type(BT[i]) == int:
-                tracker = False
-                break
-        if tracker: return "TIE"
-        else: return "NONE"
+def EndgameChecker(BT):
+    if (BT[0] == BT[1] and BT[1] == BT[2]) or (BT[3] == BT[4] and BT[4] == BT[5]) or (BT[6] == BT[7] and BT[7] == BT[8]): return True
+    elif (BT[0] == BT[3] and BT[3] == BT[6]) or (BT[1] == BT[4] and BT[4] == BT[7]) or (BT[2] == BT[5] and BT[5] == BT[8]): return True
+    elif (BT[0] == BT[4] and BT[4] == BT[8]) or (BT[2] == BT[4] and BT[4] == BT[6]): return True
+    else: return False
         
-def play_again():
+def PlayAgain():
     answer = input("Would you like to play again (Y/N): ")
     if answer == "Y" or answer == "y": return intro()
     if answer == "N" or answer == "n":
@@ -171,20 +169,20 @@ def play_again():
         print("******** THANKS FOR PLAYING! ********")
     else:
         print("ERROR: Respond with 'Y' or 'N'")
-        return play_again()
+        return PlayAgain()
     
 def win(player):
     print("*************************************")
     print("*--------- PLAYER " + str(player) + " WINS -----------*")
     print("*************************************")
     print()
-    play_again()
+    PlayAgain()
 
 def tie():
     print("*************************************")
     print("*----------- ITS A TIE! ------------*")
     print("*************************************")
     print()
-    play_again()  
+    PlayAgain()  
 
 intro()
